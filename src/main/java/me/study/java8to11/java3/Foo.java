9bfe6eb9b83cfe5ab8b967ff6446f7cd50b9943b
Jihye Han <hangji0124@gmail.com>
2022-09-30 00:08:23 +0900

    BiFunction, Consumer 인터페이스

--- src/main/java/me/study/java8to11/java3/Foo.java
+++ src/main/java/me/study/java8to11/java3/Foo.java
@@ -1,27 +1,29 @@
 package me.study.java8to11.java3;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Foo {
     public static void main(String[] args) {
-        // Integer 타입을 받아 Integer 타입을 리턴하는 함수 인터페이스 R apply(T t)
-        Function<Integer, Integer> plus10 = (number) -> number + 10;
-        Function<Integer, Integer> multiply2 = (i) -> i*2;
-
-        // 함수를 조합
-        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
-        System.out.println(multiply2AndPlus10.apply(2));
-
-        Function<Integer, Integer> plus10AndMultiply2 = plus10.andThen(multiply2);
-        System.out.println(plus10AndMultiply2.apply(2));
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10);
         /**
          * 기본 함수 메서드
          * R apply(T t)
-         * Integer 타입을 받아 Integer 타입을 리턴하는 함수 인터페이스
+         * T 타입을 받아 R 타입을 리턴하는 함수 인터페이스
          *
          * 함수 조합용 메서드 -> 고차함수
          * andThen
          * compose
+         *
+         * BiFunction<T, U, R>
+         *     R apply(T t, U u)
+         * 두개의 값을 받아서 R타입을 리턴하는 함수 인터페이스
+         *
+         * Consumer<T>
+         *     void accept(T t)
+         * T 타입을 받아 아무값도 리턴하지 않는 함수
+         *
          */
     }
 }
