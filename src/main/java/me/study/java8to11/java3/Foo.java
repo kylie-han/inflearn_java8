aa93eade56a628698236b7bd5b137bb98e2cb4a0
Jihye Han <hangji0124@gmail.com>
2022-09-29 23:50:10 +0900

    자바에서 제공하는 함수형 인터페이스를 사용

--- src/main/java/me/study/java8to11/java3/Foo.java
+++ src/main/java/me/study/java8to11/java3/Foo.java
@@ -1,8 +1,11 @@
 package me.study.java8to11.java3;
 
+import java.util.function.Function;
+
 public class Foo {
     public static void main(String[] args) {
-        Plus10 plus10 = new Plus10();
-        System.out.println(plus10.apply(1));
+        // Integer 타입을 받아 Integer 타입을 리턴하는 함수 인터페이스 R apply(T t)
+        Function<Integer, Integer> plus10 = (number) -> number + 10;
+        System.out.println(plus10.apply(10));
     }
 }
